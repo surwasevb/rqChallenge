a602bf8b9974ccbb83b33972e3aa14683f56b687
surwasevb <surwasevbit@gmail.com>
2025-02-12 12:22:58 +0530

    add custom exception classes

---
+++ api/src/main/java/com/reliaquest/api/exception/ServerErrorException.java
@@ -0,0 +1,8 @@
+package com.reliaquest.api.exception;
+
+public class ServerErrorException extends RuntimeException {
+
+    public ServerErrorException(String message) {
+        super(message);
+    }
+}
